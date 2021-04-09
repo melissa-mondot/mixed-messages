dbb528aae39dc1a6474925de0b00e3173f5e5683
Melissa Mondot <31637708+melissa-mondot@users.noreply.github.com>
2021-04-08 22:30:11 -0700

    Add plan for pig latin generator function

---
+++ index.js
@@ -0,0 +1,16 @@
+// pig latin generator
+// takes a string as a parameter
+// within function:
+//  split words into array
+//  iterate
+//  if any are a number, change to string
+// map through new array with pig latin function
+
+// pig latin function
+//  if begins with consonant
+//      newString var
+//      slice [0:first vowel] and set to var
+//      append slice to end of string
+//
+//  append "ay" to end of new string
+//  return newString
