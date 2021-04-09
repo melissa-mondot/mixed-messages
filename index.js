2952dddd38634429c6c7ef3109b99fb7d5512f51
Melissa Mondot <31637708+melissa-mondot@users.noreply.github.com>
2021-04-08 22:58:45 -0700

    Flow for two functions responding correctly

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 //  split words into array
 //  iterate
 //  if any are a number, change to string
+// TODO: looking for way to detect numerals -- typeof = "number" does not work as the numeral is within a string
 // map through new array with pig latin function
 
 // pig latin function
@@ -14,3 +15,20 @@
 //
 //  append "ay" to end of new string
 //  return newString
+
+const pigLatinGenerator = (str) => {
+  const strArr = str.split(" ");
+  const newStr = converter(strArr);
+  const finalStr = newStr.join(" ");
+
+  return finalStr;
+};
+
+const converter = (arr) => {
+  console.log("converting array now");
+  return arr;
+};
+
+const testStr = "Hello my name is Simon.";
+
+console.log(pigLatinGenerator(testStr));
